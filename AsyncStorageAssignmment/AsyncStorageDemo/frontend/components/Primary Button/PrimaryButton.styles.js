2ea068ba77ef88cdaba6a95a36bdf628c02d4094
DeherNaqvi <dehernaqvi@gmail.com>
2025-11-12 21:57:46 +0500

    Fixed submodule issue - added full project folders

---
+++ AsyncStorageAssignmment/AsyncStorageDemo/frontend/components/Primary Button/PrimaryButton.styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  button: {
+    borderRadius: 12,
+    paddingVertical: 12,
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontWeight: '600',
+    fontSize: 15,
+  },
+});
